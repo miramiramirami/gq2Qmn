ae06f95fd25f31ee65f6426982771b56dfadf449
miramiramirami <xeless@mail.ru>
2026-01-13 10:03:18 +0500

    init

---
+++ back/app/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    app_name: str = 'Fast API sp'
+    debug: bool = True
+    database_url: str = 'sqlite:///./shop.db'
+    cors_origins: list = [
+        'http://localhost:5173',
+        'http://localhost:3000',
+        'http://127.0.0.1:5173',
+        'http://127.0.0.1:3000',
+    ]
+    static_dir: str = 'static'
+    images_dirs: str = 'static/images'
+
+    class Config:
+        env_file = '.env'
+
+
+settings = Settings()
